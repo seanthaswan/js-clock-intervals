5ead009b03ef599405220d07274014c635ced2ce
seanthaswan <owneruser@Owners-MacBook-Pro.local>
2017-12-01 09:04:23 -0800

    Achieved objectives, started on bonus but now running into errors regarding setting minutes and hours to current time.. yay

---
+++ clock.js
@@ -0,0 +1,58 @@
+var second = document.getElementById("second");
+var minute = document.getElementById("minute");
+var hour = document.getElementById("hour");
+
+
+//TRYING BONUS
+var currentDate = new Date();
+var seconds = currentDate.getSeconds();
+var minutes = currentDate.getMinutes();
+var hours = currentDate.getHours();
+
+
+
+// var minuteRotation = function(){
+// 	minutes++;
+// 	var minuteDegrees = (minutes/60)*360;
+// 	minute.style.transform = "rotate(" + minuteDegrees + "deg)";
+// };
+
+
+
+
+var secondRotation = function(){
+// seconds++;
+var secondDegrees = (seconds/60)*360;
+second.style.transform = "rotate(" + secondDegrees + "deg)";
+// minutes++;
+var minuteDegrees = (minutes/60)*360;
+minute.style.transform = "rotate(" + minuteDegrees + "deg)";
+}
+var hourRotation = function(){
+// hours++;
+var hourDegrees = (hours/60)*12;
+hour.style.transform = "rotate(" + hourDegrees + "deg)";
+}
+// setInterval(secondRotation, 1000);
+// setInterval(minuteRotation, 60000);
+// setInterval(hourRotation, 360000);
+
+
+// };
+// second.style.transform = "rotate(" + degrees + "deg)";
+// minute.style.transform = "rotate(" + degrees + "deg)";
+// hour.style.transform = "rotate(" + degrees + "deg)";
+// };
+
+
+var checkCurrentTime = function(){
+	currentDate = new Date();
+	seconds = currentDate.getSeconds();
+	minutes = currentDate.getMinutes();
+	hours = currentDate.getHours();
+	secondRotation();
+	minuteRotation();
+	hourRotation();
+}
+
+setInterval(checkCurrentTime, 1000);
